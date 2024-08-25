3587cffcd9c2eef6cdfcf0b7108841acd1924f32
Elivelton Almeida <95354880+EliveltonProgrammer@users.noreply.github.com>
2024-08-25 10:11:20 -0300

    Update DALEmpresa.cs

--- DAL/DALEmpresa.cs
+++ DAL/DALEmpresa.cs
@@ -1,1 +1,35 @@
+public List<Dictionary<string, string>> GetEmpresasValidationLicense()
+{
+    List<Dictionary<string, string>> empresas = new List<Dictionary<string, string>>();
 
+    try
+    {
+        using (SqlConnection connection = new SqlConnection(Conexao.Connection.connectionString))
+        {
+            connection.Open();
+
+            string sql = "SELECT EmprCod, EmprNome, EmprCnpj FROM Empresas";
+
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Dictionary<string, string> empresa = new Dictionary<string, string>();
+                        empresa["EmprCod"] = reader["EmprCod"].ToString();
+                        empresa["EmprNome"] = reader["EmprNome"].ToString();
+                        empresa["EmprCnpj"] = reader["EmprCnpj"].ToString();
+                        empresas.Add(empresa);
+                    }
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show("Ocorreu uma exceção: " + ex.Message);
+    }
+
+    return empresas;
+}
